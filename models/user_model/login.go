2b63cf23faf260f906ec2ae7db86310dad2b9cd5
suzhanwei <545397649@qq.com>
2018-04-19 21:19:44 +0800

    优化

--- models/user_model/login.go
+++ models/user_model/login.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"logs"
-	"models"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
@@ -12,7 +11,7 @@ import (
 
 func Get(uname string, pword int64) (ret map[string]string, err error) {
 	var engine *xorm.Engine
-	users := make([]models.UserLogin, 0)
+	users := make([]UserLogin, 0)
 	var e error
 	if engine, e = xorm.NewEngine("mysql", "work:123456@tcp(39.107.94.42:3306)/go?charset=utf8"); e == nil {
 		if err = engine.Where("name=? and password=?", uname, pword).Find(&users); err != nil {
--- models/user_model/register.go
+++ models/user_model/register.go
@@ -3,7 +3,6 @@ package models
 import (
 	"logs"
 	"math/rand"
-	"models"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -12,10 +11,10 @@ import (
 
 //用户信息
 
-func Register(param models.RegisterParam) (ret map[string]string, err error) {
+func Register(param RegisterParam) (ret map[string]string, err error) {
 	var engine *xorm.Engine
 	ret = make(map[string]string)
-	var user models.UserLogin
+	var user UserLogin
 	if engine, err = xorm.NewEngine("mysql", "work:123456@tcp(39.107.94.42:3306)/go?charset=utf8"); err != nil {
 		logs.Error.Println("engine error", err)
 	}
