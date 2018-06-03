ad5c9262aa650cb327f233cdf0042da43563fe07
suzhanwei <545397649@qq.com>
2018-06-03 23:18:00 +0800

    manange

--- models/manage_model/add.go
+++ models/manage_model/add.go
@@ -14,6 +14,12 @@ func Add(p AddParam) (ret string, err error) {
 	pi.Position = p.Position
 	pi.SecondPosition = p.SecondPosition
 	pi.Type = p.Type
+	pi.Score = p.Score
+	pi.Rebound = p.Rebound
+	pi.Assist = p.Assist
+	pi.Steal = p.Steal
+	pi.Cap = p.Cap
+	pi.AppearNum = p.AppearNum
 	if engine, e := mysql.GetEngine(); e == nil {
 		if _, e1 := engine.InsertOne(&pi); e != nil {
 			logs.Error.Println("db :err ", e1)
--- models/struct/player_info.go
+++ models/struct/player_info.go
@@ -1,10 +1,16 @@
 package strucct
 
 type PlayerInfo struct {
-	Pid            int64  `xorm:"not null pk autoincr BIGINT(20)"`
+	PlayerId       int64  `xorm:"not null pk autoincr BIGINT(20)"`
 	Name           string `xorm:"not null default '' unique(uk_name) VARCHAR(128)"`
 	NickName       string `xorm:"not null default '' unique(uk_name) VARCHAR(128)"`
 	Position       int    `xorm:"not null unique(uk_type_position) INT(11)"`
 	SecondPosition int    `xorm:"not null default 0 INT(11)"`
 	Type           int    `xorm:"not null default 0 unique(uk_type_position) INT(11)"`
+	Score          int    `xorm:"not null default 0 INT(11)"`
+	Rebound        int    `xorm:"not null default 0 INT(11)"`
+	Assist         int    `xorm:"not null default 0 INT(11)"`
+	Steal          int    `xorm:"not null default 0 INT(11)"`
+	Cap            int    `xorm:"not null default 0 INT(11)"`
+	AppearNum      int    `xorm:"not null default 0 INT(11)"`
 }
